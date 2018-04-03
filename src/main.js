bd0aee46dd10446188f61fc9535015dc34ffbf26
Jairo Camayo <asnete@gmail.com>
2018-04-04 01:35:35 +0200

    schedule table update - main js updated

--- src/main.js
+++ src/main.js
@@ -1,14 +1,113 @@
-var moment = require('moment');
+  var initApp = function(){
+    var dayContent = document.getElementById("day-content");
+    var moment = require('moment');
 
-var currentDate = moment();
-var weekStart = currentDate.clone().startOf('week');
-var weekEnd = currentDate.clone().endOf('week');
+    var currentDate = moment();
+    var weekStart = currentDate.clone().startOf('week');
+    var weekEnd = currentDate.clone().endOf('week');
 
-var days = [];
-for (i = 0; i <= 6; i++) {
+    var days = [];
+    for (i = 0; i <= 6; i++) {
+       days.push(moment(weekStart).add(i, 'days'));
+    };
+    for (i = 0; i < days.length; i++) {
+      if (i === 0) {
+        dayContent.innerHTML += '<div class="columns">' +
+              '<div class="column">' +
+                '<div class="columns">' +
+                  '<div class="column is-3">' +
+                    '<div>Day</div>' +
+                    '<div>' + days[i].format("ddd") + ' ' + days[i].format("D") + '</div>' +
+                  '</div>' +
+                  '<div class="column">' +
+                    '<div class="columns">' +
+                      '<div class="column is-2 has-text-centered">' +
+                        '<div>time</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                      '</div>' +
+                      '<div class="column">' +
+                        '<div>description</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                      '</div>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+              '<div class="column">' +
+                '<div class="columns">' +
+                  '<div class="column">' +
+                    '<div>task</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                  '</div>' +
+                  '<div class="column is-2 has-text-centered">' +
+                    '<div>done</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+            '</div>';
+      } else {
+        dayContent.innerHTML += '<div class="columns">' +
+              '<div class="column">' +
+                '<div class="columns">' +
+                  '<div class="column is-3">' +
+                    '<div>' + days[i].format("ddd") + ' ' + days[i].format("D") + '</div>' +
+                  '</div>' +
+                  '<div class="column">' +
+                    '<div class="columns">' +
+                      '<div class="column is-2 has-text-centered">' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                      '</div>' +
+                      '<div class="column">' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                        '<div>-</div>' +
+                      '</div>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+              '<div class="column">' +
+                '<div class="columns">' +
+                  '<div class="column">' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                  '</div>' +
+                  '<div class="column is-2 has-text-centered">' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                    '<div>-</div>' +
+                  '</div>' +
+                '</div>' +
+              '</div>' +
+            '</div>';
+      }
+    }
+    console.log(days);
+    console.log(moment('01/19/2016').format("MMMM Do,dddd"));
+  };
 
-   days.push(moment(weekStart).add(i, 'days').format("MMMM Do,dddd"));
+  if (
+      document.readyState === "complete" ||
+      (document.readyState !== "loading" && !document.documentElement.doScroll)
+  ) {
+    initApp();
+  } else {
+    document.addEventListener("DOMContentLoaded", initApp);
+  }
 
-};
-console.log(days);
-console.log(moment('01/19/2016').format("MMMM Do,dddd"));
